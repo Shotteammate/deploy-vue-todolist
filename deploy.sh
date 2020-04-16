33207d101106460e3bc547795796d4a06ec567b1
shotteammate <shotteammate@gmail.com>
2020-04-16 17:52:14 +0800

    first commit

--- deploy.sh
+++ deploy.sh
@@ -21,6 +21,6 @@ git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:Shotteammate/vue-todolist.git master:gh-pages
-git push -f https://github.com/Shotteammate/vue-todolist.git master:gh-pages
+git push -f https://github.com/Shotteammate/deploy-vue-todolist.git master:gh-pages
 
 cd -
